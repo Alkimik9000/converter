a364fb97301d14ad8bc5f7de38ab3cf65cf130a1
Mark Ofir <markofir@Marks-MacBook-Pro.local>
2023-07-24 20:42:30 +0300

    adding api

--- converter.py
+++ converter.py
@@ -1,20 +1,30 @@
 from tkinter import *
+
+def dollar_nis():
+    dollar = float(dollar_input.get())
+    nis = dollar * 3.66
+    money_resule_label.config(text=nis)
+
+
 window = Tk()
 window.title("Money Converter")
 window.minsize(500,300)
 
-dollar_input = Entry()
-dollar_input.pack()
+dollar_input = Entry(width=7)
+dollar_input.grid(column=1, row=0)# api
 
 dollar_label = Label(text="Enter Dollar amount $")
-dollar_label.pack()
+dollar_label.grid(column=0, row=0)
 
 is_equal_label = Label(text="is equal to ")
-is_equal_label.pack()
+is_equal_label.grid(column=0, row=2)
 
 money_resule_label = Label(text="0")
-money_resule_label.pack()
+money_resule_label.grid(column=1, row=2)
 
 nis_label = Label(text="NIS")
-nis_label.pack()
+nis_label.grid(column=2, row=2)
+
+calculate_button = Button(text="calculate", command=dollar_nis)
+calculate_button.grid(column=1 ,row=3)
 window.mainloop()
